227dabce46cabd2378b03fd37d2d701e248ad5f4
decipherthecode <113307200+decipherthecode@users.noreply.github.com>
2022-09-11 23:48:13 +0530

    Add files via upload

---
+++ Brand name Generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+print("Welcome to the Band name Generator")
+city_name = input("What's is city name you grew up in? \n")
+pet_name = input("What's your pet's name? \n")
+print(city_name + " " + pet_name)
+
+
+# In[ ]:
+
+
+
+
